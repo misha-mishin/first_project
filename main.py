01e7b3deac22f8bf802079b586e1ce62b275a31a
MikhailKolychev <cdplayer3000@ya.ru>
2025-10-29 00:06:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello from repository!')
+
+def print_author():
+    # Импорт load_dotenv.
+    from dotenv import load_dotenv 
+
+    # Импорт библиотеки для работы с окружением.
+    import os  
+
+    # Загрузка переменных из .env
+    load_dotenv(dotenv_path='/Users/mishamishin/Desktop/Data Science/Практикум/Проекты/Спринт 8/second_new_project/first_project/.env')
+
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+print_author()
